e07524e3c41183ec9a97e7648571f122d94bfb8d
codeamac07 <codeamac07@gmail.com>
2015-09-21 11:05:49 -0500

    the last part transforms an array of arrays into an array of hashes

--- gato.rb
+++ gato.rb
@@ -10,4 +10,18 @@ def tablero_gato
     puts "----".center(24, "-")
   end
 end
-tablero_gato
+#tablero_gato
+
+titulos = ["Nombre", "Edad", "Genero", "Grupo", "Calificaciones"]
+calis = [["Rodrigo García", 13, "Masculino", "Primero", [9,9,7,6,8]],
+["Fernanda González", 12, "Femenino", "Tercero", [6,9,8,6,8]],
+["Luis Pérez", 13, "Masculino", "Primero", [8,7,7,9,8]],
+["Ana Espinosa", 14, "Femenino", "Segundo", [9,9,6,8,8]],
+["Pablo Moran", 11, "Masculino", "Segundo", [7,8,9,9,8]]]
+
+aux = []
+(calis.length).times do |x|
+   arr = titulos.zip(calis[x])
+   aux.push(Hash[arr])
+ end
+p aux
